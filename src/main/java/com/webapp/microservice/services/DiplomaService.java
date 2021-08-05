788ef8f85e7eb21f939644b5192245d4525cacfd
Dennis Urrutia <dennis.urrutia@usach.cl>
2021-08-05 19:21:18 -0400

    feat: UploadFile
    subida de múltiples archivos y descarga de archivos

--- src/main/java/com/webapp/microservice/services/DiplomaService.java
+++ src/main/java/com/webapp/microservice/services/DiplomaService.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @CrossOrigin
 @RestController
-@RequestMapping(value = "diplomas")
+@RequestMapping(value = "/diplomas")
 public class DiplomaService{
 
     private final DiplomaRepository diplomaRepository;
--- src/main/java/com/webapp/microservice/services/UploadFileService.java
+++ src/main/java/com/webapp/microservice/services/UploadFileService.java
@@ -1,9 +1,14 @@
 package com.webapp.microservice.services;
 
+import com.webapp.microservice.repositories.UploadFiles;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.File;
@@ -11,32 +16,53 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import com.google.gson.Gson;
-
-
+import java.util.List;
+import java.util.stream.Collectors;
 
 @CrossOrigin
 @RestController
-@RequestMapping(value = "/upload")
+@RequestMapping(value = "/uploadfiles")
 public class UploadFileService {
 
-    private final Gson gson;
 
-    public UploadFileService(Gson gson) {
-        this.gson = gson;
+    @Autowired
+    private UploadFiles uploadFiles;
+
+    @PostMapping(value="/upload")
+    public ResponseEntity<String> uploadFiles(@RequestParam("files") List<MultipartFile> files){
+        try {
+            uploadFiles.save(files);
+            return ResponseEntity.status(HttpStatus.OK).body("Los archivos fueron cargados correctamente al servidor");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("El/los archivo/s ya existe/n");
+        }
     }
 
-    @GetMapping(value="/")
-    public String index(){
-        return "file";
+    @GetMapping(value="/fies/{filename:.+}")
+    public ResponseEntity<Resource> getFile(@PathVariable String fileName) throws Exception {
+        Resource resource = uploadFiles.load(fileName);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attchment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
 
-    @PostMapping(value = "/file")
-    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file, RedirectAttributes attributes) throws IOException {
+    @GetMapping(value = "/getall")
+    public ResponseEntity<List<File>> getAllFiles() throws Exception {
+        List<File> files = uploadFiles.loadAll().map(path -> {
+            String filename = path.getFileName().toString();
+            String url = MvcUriComponentsBuilder.fromMethodName(UploadFileService.class, "getFile", path.getFileName().toString()).build().toString();
+            return new File(filename, url);
+        }).collect(Collectors.toList());
+        return ResponseEntity.status(HttpStatus.OK).body(files);
+    }
+
+    @PostMapping(value = "/file", consumes = {"multipart/form-data"})
+    public ResponseEntity<String> uploadFile(@RequestParam("files") MultipartFile file, RedirectAttributes attributes) throws IOException {
         try{
             if (file == null || file.isEmpty()) {
                 attributes.addFlashAttribute("message", "Por favor seleccione un archivo");
-                return new ResponseEntity<>( gson.toJson("Algo ocurrio mal :("), HttpStatus.BAD_REQUEST );
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Algo ocurrió mal 2");
             }
             StringBuilder builder = new StringBuilder();
             builder.append(System.getProperty("user.home"));
@@ -50,15 +76,10 @@ public class UploadFileService {
             Files.write(path, fileBytes);
 
             attributes.addFlashAttribute("message", "Archivo cargado correctamente");
-            return new ResponseEntity<>(gson.toJson(builder), HttpStatus.OK);
+            return ResponseEntity.status(HttpStatus.OK).body("OK");
         }catch ( Exception e ) {
             System.err.println( e ); // capturar error server-side
-            return new ResponseEntity<>( gson.toJson("Algo ocurrio mal :("), HttpStatus.BAD_REQUEST );
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Algo ocurrió mal 3");
         }
-    }
-
-    @GetMapping(value = "/status")
-    public String status(){
-        return "status";
     }
 }
