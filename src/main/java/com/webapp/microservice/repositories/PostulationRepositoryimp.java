fb589effcf5cbbea3e5ef7f03858d5790ebaf19b
Benjamin Belmar <benjamin.belmar@usach.cl>
2021-08-07 18:33:22 -0400

    refactor testing: PostulationRepository

--- src/main/java/com/webapp/microservice/repositories/PostulationRepositoryimp.java
+++ src/main/java/com/webapp/microservice/repositories/PostulationRepositoryimp.java
@@ -17,26 +17,39 @@ public class PostulationRepositoryimp implements PostulationRepository {
     @Override
     public int countPostulation() {
         int total = 0;
-        try(Connection conn = sql2o.open()){
+        Connection conn = sql2o.open();
+        try(conn){
             total = conn.createQuery("SELECT COUNT(*) FROM postulation").executeScalar(Integer.class);
+            return total;
         }
-        return total;
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            return -1;
+        }
+        finally {
+            conn.close();
+        }
     }
 
     @Override
     public List<Postulation> getAllPostulation() {
-        try(Connection conn = sql2o.open()){
+        Connection conn = sql2o.open();
+        try(conn){
             return conn.createQuery("select * from postulation")
                     .executeAndFetch(Postulation.class);
         } catch (Exception e) {
             System.out.println(e.getMessage());
             return null;
+        }finally {
+            conn.close();
         }
+
     }
 
     @Override
     public Postulation createPostulation(Postulation postulation) {
-        try(Connection conn = sql2o.open()){
+        Connection conn = sql2o.open();
+        try(conn){
             int insertedId = countPostulation()+1;
             conn.createQuery("INSERT INTO postulation (id, id_diploma, id_postulant, id_evaluator, id_secretary, status)"+
             "values (:id, :postulationId_diploma, :postulationId_postulant, :postulationId_evaluator, :postulationId_secretary, :postulationId_status)", true)
@@ -52,31 +65,38 @@ public class PostulationRepositoryimp implements PostulationRepository {
         }catch(Exception e){
             System.out.println(e.getMessage());
             return null;
+        }finally {
+            conn.close();
         }
-        
     }
+        
+
     @Override
     public boolean deletePostulation(int id){
-        try(Connection conn = sql2o.open()){
+        Connection conn = sql2o.open();
+        try(conn){
             conn.createQuery("DELETE FROM postulation WHERE id = :id").addParameter("id", id)
             .executeUpdate();
             return true; 
         }catch(Exception e){
             System.out.println(e.getMessage());
             return false;
+        }finally {
+            conn.close();
         }
 
     }
     @Override
     public void updatePostulation(int id, Postulation postulation){
         String updateSql = "update postulation set status = :status where id = :idParam";
-        try (Connection con = sql2o.open()) {
+        Connection conn = sql2o.open();
+        try (conn) {
             // search postulant by id (if exists)
-            Postulation oldPostulation = con.createQuery( "SELECT * FROM postulation where id = :idPostulation" )
+            Postulation oldPostulation = conn.createQuery( "SELECT * FROM postulation where id = :idPostulation" )
                 .addParameter("idPostulation", id)
                 .executeAndFetchFirst( Postulation.class);
             // generate query for update a postulant
-            Query query = con.createQuery(updateSql);
+            Query query = conn.createQuery(updateSql);
             query.addParameter("idParam", id);
             if ( postulation.getStatus() != null ) query.addParameter("status", postulation.getStatus());
             else query.addParameter("status", oldPostulation.getStatus());
@@ -85,6 +105,8 @@ public class PostulationRepositoryimp implements PostulationRepository {
             System.out.println("Postulation -status- has been successfully updated");
         }catch(Exception e){
             System.out.println(e.getMessage());
+        }finally {
+            conn.close();
         }
 
     }
@@ -92,15 +114,18 @@ public class PostulationRepositoryimp implements PostulationRepository {
     @Override
     public Postulation getPostulation(int id){
 		String sql = "SELECT * FROM postulation where id=:id";
-
-		try (Connection con = sql2o.open()) {
-			return con.createQuery(sql)
+        Connection conn = sql2o.open();
+		try (conn) {
+			return conn.createQuery(sql)
 				.addParameter("id", id)
 				.executeAndFetchFirst(Postulation.class);
 		}catch(Exception e){
             System.out.println(e.getMessage());
             return null;
+        }finally {
+            conn.close();
         }
-	}
+
+    }
 
 }
